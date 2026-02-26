fb3df746879a6218250549c4ce96189ecdc9c607
Jerry <junhyuk.dev@gmail.com>
2026-02-26 21:29:21 +0900

    fix: 현재접속자 수 디자인 수정

--- src/pages/signal-chat/modules/header.tsx
+++ src/pages/signal-chat/modules/header.tsx
@@ -16,21 +16,23 @@ export default function Header() {
   });
 
   return (
-    <div className="bg-foreground/95 border-border/50 fixed top-4 left-4 z-50 rounded-2xl border px-6 py-4 shadow-lg backdrop-blur-sm">
-      <div className="flex items-center gap-3">
-        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-white px-1">
-          <img src={logoPng} alt="logo" className="max-w-full" />
-        </div>
-        <div className="flex flex-col items-center gap-1">
-          <p className="text-contrast text-sm font-semibold">관리자 시그널 채팅</p>
-          <div className="flex items-center gap-1 rounded-sm bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700">
-            <Users className="h-4 w-4 text-blue-500" />
-            <span>{`현재 접속자: ${activeUserCount?.connected_count || 0}명`}</span>
+    <div className="bg-foreground/95 border-border/50 fixed top-4 left-4 z-50 rounded-2xl border px-4 py-4 shadow-lg backdrop-blur-sm">
+      <div className="flex-row items-center">
+        <div className="border-border mb-2 flex items-center gap-3 border-b pb-2">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-white px-1">
+            <img src={logoPng} alt="logo" className="max-w-full" />
           </div>
+          <div className="flex flex-col gap-1">
+            <p className="text-contrast text-sm font-semibold">관리자 시그널 채팅</p>
+            <p className="text-contrast text-xs">메시지를 작성하고 전달하세요.</p>
+          </div>
+          {/* <ThemeToggle /> */}
         </div>
-        {/* <ThemeToggle /> */}
+        <div className="flex items-center justify-center gap-2 bg-blue-50 py-1 text-xs font-medium text-blue-700">
+          <Users className="h-4 w-4 text-blue-500" />
+          <span>{`현재 ${activeUserCount?.connected_count || 0}명 접속중`}</span>
+        </div>
       </div>
-      <div className="mt-1 flex justify-center"></div>
     </div>
   );
 }
